dfbf4bcf7f4be66e3184a738c5561726d10fe6b8
Zohar <zoharmshaked@gmail.com>
2026-03-04 10:12:08 +0200

    add project structure

---
+++ leetcode/palindrome_number.py
@@ -0,0 +1,18 @@
+def isPalindrome(x):
+    if x==0:
+        return True
+    if x<0:
+        return False
+    if x%10==0:
+        return False
+    z=x
+    y=0
+    while x!=0 :
+        y=y*10+x%10
+        x=x//10
+
+    return y==z
+
+
+print(isPalindrome(59695))
+        
